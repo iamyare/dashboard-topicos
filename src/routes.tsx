714271ad8632c2861d1ab21c1a4293b4fcbd1bb4
Yamir Alejandro Rodas Elvir <79547618+iamyare@users.noreply.github.com>
2025-02-12 20:25:07 -0600

    Eliminar comentarios de error de TypeScript y limpiar el código en componentes de gráficos

--- src/routes.tsx
+++ src/routes.tsx
@@ -18,37 +18,37 @@ const routes = [
     path: 'default',
     icon: <MdHome className="h-6 w-6" />,
   },
-  {
-    name: 'NFT Marketplace',
-    layout: '/admin',
-    path: 'nft-marketplace',
-    icon: <MdOutlineShoppingCart className="h-6 w-6" />,
+  // {
+  //   name: 'NFT Marketplace',
+  //   layout: '/admin',
+  //   path: 'nft-marketplace',
+  //   icon: <MdOutlineShoppingCart className="h-6 w-6" />,
 
-    secondary: true,
-  },
-  {
-    name: 'Data Tables',
-    layout: '/admin',
-    icon: <MdBarChart className="h-6 w-6" />,
-    path: 'data-tables',
-  },
-  {
-    name: 'Profile',
-    layout: '/admin',
-    path: 'profile',
-    icon: <MdPerson className="h-6 w-6" />,
-  },
-  {
-    name: 'Sign In',
-    layout: '/auth',
-    path: 'sign-in',
-    icon: <MdLock className="h-6 w-6" />,
-  },
-  {
-    name: 'RTL Admin',
-    layout: '/rtl',
-    path: 'rtl-default',
-    icon: <MdHome className="h-6 w-6" />,
-  },
+  //   secondary: true,
+  // },
+  // {
+  //   name: 'Data Tables',
+  //   layout: '/admin',
+  //   icon: <MdBarChart className="h-6 w-6" />,
+  //   path: 'data-tables',
+  // },
+  // {
+  //   name: 'Profile',
+  //   layout: '/admin',
+  //   path: 'profile',
+  //   icon: <MdPerson className="h-6 w-6" />,
+  // },
+  // {
+  //   name: 'Sign In',
+  //   layout: '/auth',
+  //   path: 'sign-in',
+  //   icon: <MdLock className="h-6 w-6" />,
+  // },
+  // {
+  //   name: 'RTL Admin',
+  //   layout: '/rtl',
+  //   path: 'rtl-default',
+  //   icon: <MdHome className="h-6 w-6" />,
+  // },
 ];
 export default routes;
